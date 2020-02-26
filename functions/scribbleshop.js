dff0cfeb75630c49208d6609a05ca5c3a6c480b2
Tom Jardine-McNamara <tom@itsravenous.com>
2020-02-26 22:28:09 +0000

    respond to slack challenge

--- functions/scribbleshop.js
+++ functions/scribbleshop.js
@@ -1,4 +1,19 @@
 exports.handler = async (event, context, callback) => {
-  console.log({event})
-};
+  console.log({ event });
+  const { challenge } = JSON.parse(
+    '{"' + event.body.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+    (key, value) =>
+      key === ""
+        ? value
+        : decodeURIComponent(value)
+            .replace(/\+/g, " ")
+            .trim()
+  );
 
+  if (challenge) {
+    return callback(null, {
+      statusCode: 200,
+      body: challenge
+    });
+  }
+};
